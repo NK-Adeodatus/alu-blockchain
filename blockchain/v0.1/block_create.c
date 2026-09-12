34ed605187944315f3dfa793fd69d65fc0ac4f7a
N-Adeodatus <nkundiman3@gmail.com>
2026-09-12 18:16:21 +0200

    Create Block

---
+++ blockchain/v0.1/block_create.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "blockchain.h"
+
+/**
+ * block_create - Creates a new Block and initialises it
+ *
+ * @prev:     Pointer to the previous Block in the Blockchain
+ * @data:     Memory area to duplicate in the Block's data
+ * @data_len: Number of bytes to duplicate from @data
+ *
+ * Return: Pointer to the allocated Block, or NULL on failure
+ */
+block_t *block_create(block_t const *prev, int8_t const *data,
+	uint32_t data_len)
+{
+	block_t		*block;
+	uint32_t	len;
+
+	block = calloc(1, sizeof(*block));
+	if (!block)
+		return (NULL);
+	block->info.index = prev->info.index + 1;
+	block->info.difficulty = 0;
+	block->info.nonce = 0;
+	block->info.timestamp = (uint64_t)time(NULL);
+	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	len = data_len > BLOCKCHAIN_DATA_MAX ? BLOCKCHAIN_DATA_MAX : data_len;
+	memcpy(block->data.buffer, data, len);
+	block->data.len = len;
+	return (block);
+}
